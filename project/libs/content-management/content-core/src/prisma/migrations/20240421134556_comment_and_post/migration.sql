8a2264aaf6cd002eb7bee06d7d93937f279fb93b
opifex <opifexM@outlook.com>
2024-04-21 10:23:40 -0400

    Refactor SQL migrations and Prisma schema to use UUIDs for posts and comments

--- project/libs/content-management/content-core/src/prisma/migrations/20240421134556_comment_and_post/migration.sql
+++ project/libs/content-management/content-core/src/prisma/migrations/20240421134556_comment_and_post/migration.sql
@@ -6,9 +6,9 @@ CREATE TYPE "DatabasePostType" AS ENUM ('VIDEO', 'TEXT', 'QUOTE', 'PHOTO', 'LINK
 
 -- CreateTable
 CREATE TABLE "comments" (
-    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "text" TEXT NOT NULL,
-    "post_id" TEXT NOT NULL,
+    "post_id" UUID NOT NULL,
     "author_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
 
@@ -17,7 +17,7 @@ CREATE TABLE "comments" (
 
 -- CreateTable
 CREATE TABLE "posts" (
-    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "tags" TEXT[],
     "author_id" TEXT NOT NULL,
     "posted_at" TIMESTAMP(3) NOT NULL,
@@ -35,83 +35,83 @@ CREATE TABLE "posts" (
 
 -- CreateTable
 CREATE TABLE "link_posts" (
-    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "url" TEXT NOT NULL,
     "description" TEXT,
-    "postId" TEXT NOT NULL,
+    "post_id" UUID NOT NULL,
 
     CONSTRAINT "link_posts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "photo_posts" (
-    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "url" TEXT NOT NULL,
-    "postId" TEXT NOT NULL,
+    "post_id" UUID NOT NULL,
 
     CONSTRAINT "photo_posts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "quote_posts" (
-    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "id" TEXT NOT NULL,
     "text" TEXT NOT NULL,
     "quote_author_id" TEXT NOT NULL,
-    "postId" TEXT NOT NULL,
+    "post_id" UUID NOT NULL,
 
     CONSTRAINT "quote_posts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "text_posts" (
-    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "title" TEXT NOT NULL,
     "announcement" TEXT,
     "text" TEXT NOT NULL,
-    "postId" TEXT NOT NULL,
+    "post_id" UUID NOT NULL,
 
     CONSTRAINT "text_posts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "video_posts" (
-    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "title" TEXT NOT NULL,
     "url" TEXT NOT NULL,
-    "postId" TEXT NOT NULL,
+    "post_id" UUID NOT NULL,
 
     CONSTRAINT "video_posts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "link_posts_postId_key" ON "link_posts"("postId");
+CREATE UNIQUE INDEX "link_posts_post_id_key" ON "link_posts"("post_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "photo_posts_postId_key" ON "photo_posts"("postId");
+CREATE UNIQUE INDEX "photo_posts_post_id_key" ON "photo_posts"("post_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "quote_posts_postId_key" ON "quote_posts"("postId");
+CREATE UNIQUE INDEX "quote_posts_post_id_key" ON "quote_posts"("post_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "text_posts_postId_key" ON "text_posts"("postId");
+CREATE UNIQUE INDEX "text_posts_post_id_key" ON "text_posts"("post_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "video_posts_postId_key" ON "video_posts"("postId");
+CREATE UNIQUE INDEX "video_posts_post_id_key" ON "video_posts"("post_id");
 
 -- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "link_posts" ADD CONSTRAINT "link_posts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "link_posts" ADD CONSTRAINT "link_posts_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "photo_posts" ADD CONSTRAINT "photo_posts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "photo_posts" ADD CONSTRAINT "photo_posts_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "quote_posts" ADD CONSTRAINT "quote_posts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "quote_posts" ADD CONSTRAINT "quote_posts_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "text_posts" ADD CONSTRAINT "text_posts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "text_posts" ADD CONSTRAINT "text_posts_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "video_posts" ADD CONSTRAINT "video_posts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "video_posts" ADD CONSTRAINT "video_posts_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
